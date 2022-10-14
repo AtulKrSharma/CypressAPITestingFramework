e174e9c3c0d49ab70241b8b91e5104afdfdeb689
Atul Kumar Sharma <sharma.atulkumar29@gmail.com>
2022-10-14 16:42:14 -0400

    This commit includes cypress config changes as well as adding new spec files

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    watchForFileChanges: false,
+    failOnStatusCode: false,
   },
 });
