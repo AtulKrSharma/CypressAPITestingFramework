8eb95d23b9c82a979255d036b3dfe6d810034412
Atul Sharma aka Pink Panther <49794348+AtulKrSharma@users.noreply.github.com>
2022-10-18 12:13:38 -0400

    added comment
    added comment

--- cypress/e2e/3-advanced-api-examples/oAuth2_Authorization_Code.cy.js
+++ cypress/e2e/3-advanced-api-examples/oAuth2_Authorization_Code.cy.js
@@ -20,6 +20,7 @@ describe("oAuth2.0", () => {
     }).then((response) => {
       expect(response.status).to.equal(200);
       JSON.stringify(response.body);
+      // hi, I'm on github.com
     });
 
     //   //Auth Token-Post
