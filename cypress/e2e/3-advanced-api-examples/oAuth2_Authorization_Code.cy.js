ca19eda8f1088abf93063429a096ce5bf496401e
Atul Sharma aka Pink Panther <49794348+AtulKrSharma@users.noreply.github.com>
2022-10-18 14:05:00 -0400

    comments added for github.com

--- cypress/e2e/3-advanced-api-examples/oAuth2_Authorization_Code.cy.js
+++ cypress/e2e/3-advanced-api-examples/oAuth2_Authorization_Code.cy.js
@@ -20,67 +20,6 @@ describe("oAuth2.0", () => {
     }).then((response) => {
       expect(response.status).to.equal(200);
     JSON.stringify(response.body);
-  // hi, I'm on local dir 
-
-    //   //Auth Token-Post
-    //   cy.api({
-    //     url: baseURLoAuth + "/token",
-    //    method: "POST",
-    //     form: true,
-    //     body: {
-    //       client_id: "AtulOAuthApp",
-    //       client_secret: "a9a988974ce7920ff678f1f32ddb745d",
-    //       grant_type: "authorization_code",
-    //       redirect_uri: "https://github.com/AtulKrSharma",
-    //       code: auth_code,
-    //     },
-    //   }).as("GetoAuthToken");
-
-    //   cy.get("@GetoAuthToken")
-    //     .then(function (respo) {
-    //       expect(respo.status).to.equal(200);
-    //       //Got the Auth token
-    //       let tokenValue = respo.body.access_token;
-    //       //cy.log(tokenValue);
-    //       return tokenValue;
-    //     })
-    //     .then((tokenValue) => {
-    //       //Booking created
-    //       cy.api({
-    //         url: baseURLoAuth + "/api/me",
-    //         method: "GET",
-    //         headers: {
-    //           "content-type": "application/x-www-form-urlencoded",
-    //           "Cache-Control": "no-cache",
-    //           followRedirect: false,
-    //           Authorization: "Bearer " + tokenValue,
-    //         },
-    //       }).as("GetUserIdbyGetCall");
-
-    //       cy.get("@GetUserIdbyGetCall")
-    //         .then((respo) => {
-    //           expect(respo.status).to.equal(200);
-    //           let userIdValue = respo.body.id;
-    //           //cy.log(userIdValue);
-    //           return userIdValue;
-    //         })
-    //         .then((userIdValue) => {
-    //           cy.log("value of userIdValue is-->" + userIdValue);
-    //           cy.api({
-    //             url: baseURLoAuth + "/api/" + userIdValue + "/barn-unlock",
-    //             method: "POST",
-    //             headers: {
-    //               Authorization: "Bearer " + tokenValue,
-    //             },
-    //           }).then((response) => {
-    //             expect(response.status).to.equal(200);
-    //             expect(response.body).to.have.property("data");
-    //             expect(response.body.action).to.eq("barn-unlock");
-    //             expect(response.body.success).to.eq(true);
-    //             expect(response.body).to.have.property("success", true);
-    //             expect(response.body).to.be.a("object");
-    //           });
-    //         });
-    //     });
+     // hi, I'm on github.com
   });
 });
